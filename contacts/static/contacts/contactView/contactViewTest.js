c1575537f0483efeba8c9d62b715b4a30867a47d
Joseph Scholle <scholle@gmail.com>
2017-07-16 01:21:35 -0400

    pre trying to test

--- contacts/static/contacts/contactView/contactViewTest.js
+++ contacts/static/contacts/contactView/contactViewTest.js
@@ -1,16 +1,36 @@
-'use strict';
-
-describe('myApp.contactView module', function() {
-
-  beforeEach(module('myApp.contactView'));
-
-  describe('contactView controller', function(){
-
-    it('should ....', inject(function($controller) {
-      //spec body
-      var contactCtrl = $controller('contactView');
-      expect(contactView).toBeDefined();
-    }));
-
+// Be descriptive with titles here. The describe and it titles combined read like a sentence.
+describe('Users factory', function() {
+  it('has a dummy spec to test 2 + 2', function() {
+    // An intentionally failing test. No code within expect() will never equal 4.
+    expect().toEqual(4);
   });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+//'use strict';
+//
+//describe('myApp.contactView module', function() {
+//
+//  beforeEach(module('myApp.contactView'));
+//
+//  describe('contactView controller', function(){
+//
+//    it('should ....', inject(function($controller) {
+//      //spec body
+//      var contactCtrl = $controller('contactView');
+//      expect(contactView).toBeDefined();
+//    }));
+//
+//  });
+//});
